fff434a1ec29fc79e01da73681b8bb5ddd8c675c
amt1st2000@gmail.com <amt1st2000@gmail.com>
2023-12-21 15:37:41 -0500

    comment section-helpPage

---
+++ scripts/help.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// get access to elements on the page that we are going to use for our questions and answers resources
+const questionsDiv = document.getElementById("questions-div");
+const questionInput =document.getElementById("question-input")
+
+
+// add event listener to the question submit button (q-submit)
+
+
+
+
+
+// our first helper method that would take the question from our 
+// question input so that we can use the question to create a new div that would display answers
+
+
+// our second helper method will take the answers from the answer input which will be created by
+//  our first helper method and display the answers for each question
+
+
+
